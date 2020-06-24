435b6992fd944932faca4ee98b8718d5a73a0e75
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-06-23 19:16:35 -0500

    maybe it works i am unsure

--- src/javascripts/components/singleMycologist/singleMycologist.js
+++ src/javascripts/components/singleMycologist/singleMycologist.js
@@ -5,12 +5,11 @@ import './singleMycologist.scss';
 
 const buildMycologist = (e) => {
   const mycologistId = e.target.closest('.card').id;
-  console.error('not working', mycologistId);
   mycologistData.getMycologistById(mycologistId)
     .then((response) => {
       const mycologist = response.data;
       const domString = `
-        <h2 class="text-center">Featured Mycologist</h2>
+        <h2 class="text-center">Featured Fun Guys</h2>
         <div class="col-12">
           <div class="card text-white bg-dark border-0 rounded-0">
             <div class="card-header">Mycologist ${mycologist.name} (Age: ${mycologist.age})</div>
