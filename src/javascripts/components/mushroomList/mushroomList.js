2c72f8749104de9e529415f1140fa7c8fddab63d
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-06-23 22:22:48 -0500

    too many mushrooms

--- src/javascripts/components/mushroomList/mushroomList.js
+++ src/javascripts/components/mushroomList/mushroomList.js
@@ -1,11 +1,25 @@
 import mushroomComponent from '../mushroom/mushroom';
 import mushroomData from '../../helpers/data/mushroomData';
+import smash from '../../helpers/data/smash';
 import utils from '../../helpers/utils';
+
+const removeShroomEvent = (e) => {
+  const mushroomId = e.target.closest('.card').id;
+  console.warn(mushroomId);
+  // actually delete this mushroom from firebase
+  smash.totallyRemoveShroomie(mushroomId)
+    .then(() => {
+      // reprint the dom (so the lil shroomie goes away)
+      // eslint-disable-next-line no-use-before-define
+      buildForest();
+      utils.printToDom('#single-myco', '');
+    })
+    .catch((err) => console.error('could not delete mushroom', err));
+};
 
 const buildForest = () => {
   mushroomData.getMushrooms()
     .then((mushrooms) => {
-      console.warn('Get Mushrooms worked!!', mushrooms);
       let domString = `
         <h2 class="text-center">Forest</h2>
         <div class="d-flex flex-wrap">
@@ -18,6 +32,7 @@ const buildForest = () => {
       domString += '</div>';
 
       utils.printToDom('#forest', domString);
+      $('body').on('click', '.delete-shroom', removeShroomEvent);
     })
     .catch((err) => console.error('get mushrooms broke :/', err));
 };
